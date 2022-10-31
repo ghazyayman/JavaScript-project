34b33e82b10f0ab7b9ad45d81733c4043fd6ed97
ghazyayman <muhghazyayman@gmail.com>
2022-10-31 12:00:02 +0800

    variables

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ var sheets = ["HTML","CSS","JS"];       // array
 var a; typeof a;                        // undefined
 var a = null;                           // value null
 
+// Variables
+var a;                          // variable
+var b = "init";                 // string
+var c = "Hi" + " " + "Joe";     // = "Hi Joe"
+var d = 1 + 2 + "3";            // = "33"
+var e = [2,3,5,8];              // array
+var f = false;                  // boolean
+var g = /()/;                   // RegEx
+var h = function(){};           // function object
+const PI = 3.14;                // constant
+var a = 1, b = 2, c = a + b;    // one line
+let z = 'zzz';                  // block scope local variable
+
 
 // switch statement
 
