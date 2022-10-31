7fc63c695d9b65b5468d17cabaa6d7e7443cd093
ghazyayman <muhghazyayman@gmail.com>
2022-10-31 11:33:24 +0800

    switch statement

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-// var tweet = prompt("Compose your tweet:" );
-// alert("You have written " + tweet.length + "character you have" + (140 - tweet.length) + " character remaining ");
+// switch statement
 
-var nama = "Ghazy";
-nama.slice(4,6);
+switch (new Date().getDay()) {      // input is current day
+    case 6:                         // if (day == 6)
+        text = "Saturday";          
+        break;
+    case 0:                         // if (day == 0)
+        text = "Sunday";
+        break;
+    default:                        // else...
+        text = "Whatever";
+    } 
+    
